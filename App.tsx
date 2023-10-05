afa3d9e98e45cfa747637e37682d73677c2a2866
Iván Lynch <ivan.lynch@outlook.com.ar>
2023-10-05 14:51:21 -0300

    Inicializacion del proyecto codepush

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import codePush from 'react-native-code-push';
+
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text>Teste</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'green',
+    flex: 1,
+  },
+});
+
+export default codePush(App);
